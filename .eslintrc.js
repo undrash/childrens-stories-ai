60568ce44567e2e89d2dbd579dce896f2112fd7f
Andrei Gaspar <andrei.gaspar@tv2.no>
2023-08-08 20:05:17 +0200

    Add configs

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+/* eslint-disable quote-props */
+
+module.exports = {
+  root: true,
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended', 
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'import',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/order': 'error',
+    'no-multiple-empty-lines': ['error'],
+    'quote-props': ['error', 'consistent-as-needed'],
+    'quotes': [2, 'single', 'avoid-escape'],
+    'indent': ['error', 2, { SwitchCase: 1 }],
+    'camelcase': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/no-unused-vars': [
+      'error', {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+  'settings': {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      'typescript': {
+        'alwaysTryTypes': true,
+      },
+    },
+  },
+};
