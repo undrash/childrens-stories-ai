fe0dec345398ffd5d0ad08ae3227b1339974bbbe
Andrei Gaspar <andrei.gaspar@tv2.no>
2023-08-20 10:22:28 +0200

    Tune debugger

--- .debug/run-lambda.ts
+++ .debug/run-lambda.ts
@@ -1,5 +1,8 @@
 import { readFile } from 'node:fs/promises';
 import * as path from 'node:path';
+import { transformPayload } from './transform-payload';
+
+process.env.API_KEY = process.env.CHILDRENS_BOOKS_API_KEY;
 
 const run = async () => {
   const [, , handlerPath, payloadPath] = process.argv;
@@ -12,7 +15,17 @@ const run = async () => {
   const payload = await readFile(payloadPath, 'utf-8');
   const payloadJson = JSON.parse(payload);
 
+  transformPayload(payloadJson);
+
   const result = await handler(payloadJson);
+
+  if (result.body) {
+    result.body = JSON.parse(result.body);
+
+    console.log(JSON.stringify(result.body, null, 2));
+
+    return;
+  }
 
   console.log(JSON.stringify(result, null, 2));
 };
