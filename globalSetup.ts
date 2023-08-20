fbe27c1b72b321693b7e552d0fe84a7198f9519d
Andrei Gaspar <andrei.gaspar@tv2.no>
2023-08-20 20:15:35 +0200

    Add more env vars to global test setup

--- globalSetup.ts
+++ globalSetup.ts
@@ -7,6 +7,11 @@ const setup = (): void => {
     API_KEY: 'test',
     IMAGE_TABLE_NAME: 'test',
     COMFY_QUEUE_URL: 'test',
+    COMFY_QUEUE_NAME: 'test',
+    ECS_SERVICE_NAME: 'test',
+    ECS_CLUSTER: 'test',
+    LATENCY_SECONDS: '60',
+    TIME_PER_MESSAGE: '15',
   };
 
   process.env = { ...process.env, ...Env };
